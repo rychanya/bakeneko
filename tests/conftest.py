4ab45d0cd53c7654c054df3f4df7e491b2dd311e
rychanya <rychanya@gmail.com>
2022-06-25 02:37:47 +0300

    🧑‍💻 add db engine fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
-from sqlalchemy import create_engine
 from testcontainers.postgres import PostgresContainer
+
+from bakeneko.db import get_engine
+from bakeneko.db.scheme import Base
 
 
 @pytest.fixture
@@ -8,7 +10,15 @@ def anyio_backend():
     return "asyncio"
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def engine():
     with PostgresContainer() as postgres:
-        yield create_engine(url=postgres.get_connection_url(), echo=True, future=True)
+        _engine = get_engine(db_url=postgres.get_connection_url(), echo=True)
+        Base.metadata.create_all(_engine)
+        yield _engine
+
+
+@pytest.fixture
+def clear_db(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
