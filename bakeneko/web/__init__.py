0b30cc228756ea004912b79b6d078f60f1b39da6
rychanya <rychanya@gmail.com>
2022-07-01 11:22:11 +0300

    🔧 add config

--- bakeneko/web/__init__.py
+++ bakeneko/web/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from bakeneko.db.scheme import Base
+from bakeneko.web.dependencies import engine_depend
 from bakeneko.web.routers import question
 
 app = FastAPI()
@@ -9,3 +11,8 @@ app.include_router(question.router)
 @app.get("/")
 async def root():
     return {"res": "ok"}
+
+
+@app.on_event("startup")
+def start_up():
+    Base.metadata.create_all(engine_depend())
--- bakeneko/web/dependencies.py
+++ bakeneko/web/dependencies.py
@@ -1,5 +1,6 @@
 from bakeneko.db import get_engine
+from bakeneko.web.config import settings
 
 
 def engine_depend():
-    return get_engine("")
+    return get_engine(settings.db_url)
