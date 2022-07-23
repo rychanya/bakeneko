663f392577c2f1fe8082ee74370feb59da6c5df9
rychanya <rychanya@gmail.com>
2022-07-24 01:42:28 +0300

    💩

--- bakeneko/web/routers/tg_web_app.py
+++ bakeneko/web/routers/tg_web_app.py
@@ -1,10 +1,12 @@
 import hashlib
 import hmac
+from typing import cast
 
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from bakeneko.bot.models import User
 from bakeneko.config import settings
 
 router = APIRouter(prefix=f"/{settings.TG_WEB_APP_MENU}")
@@ -16,7 +18,8 @@ templates = Jinja2Templates(directory="/code/bakeneko/templates")
 def root(request: Request):
     return templates.TemplateResponse(name="index.jinja", context={"request": request})
 
-def check_init_data(request: Request)->None:
+
+def check_init_data(request: Request) -> None:
     data_check_string = "\n".join(
         [
             f"{k}={request.query_params[k]}"
@@ -31,11 +34,49 @@ def check_init_data(request: Request)->None:
         secret_key, data_check_string.encode(), hashlib.sha256
     ).hexdigest()
     if data_check != request.query_params.get("hash"):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="init data invalid")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="init data invalid"
+        )
 
 
-@router.post("/", dependencies=[Depends(check_init_data)])
-async def root_post(request: Request):
-    user = request.query_params.get("user")
-    print(type(user), user)
+class CheckInitData:
+    _error = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="init data invalid"
+    )
+
+    def __init__(
+        self,
+        auth_date: str | None = None,
+        hash: str | None = None,
+        query_id: str | None = None,
+        user: str | None = None,
+        can_send_after: str | None = None,
+    ) -> None:
+        if hash is None or auth_date is None or query_id is None or user is None:
+            raise self._error
+        init_data = cast(
+            filter[str],
+            filter(
+                lambda v: v is not None, [auth_date, query_id, user, can_send_after]
+            ),
+        )
+        data_check_string = "\n".join(sorted(init_data))
+        secret_key = hmac.new(
+            "WebAppData".encode(), settings.TG_TOKEN.encode(), hashlib.sha256
+        ).digest()
+        data_check = hmac.new(
+            secret_key, data_check_string.encode(), hashlib.sha256
+        ).hexdigest()
+        if data_check != hash:
+            raise self._error
+
+        self.query_id = query_id
+        self.user = User.parse_raw(user)
+
+
+@router.post("/", )
+async def root_post(init_data: CheckInitData = Depends()):
+    # user = request.query_params.get("user")
+    # print(type(user), user)
+    print(init_data.query_id, init_data.user)
     return "ok"
