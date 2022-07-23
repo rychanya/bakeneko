1e07d14887d2e52f9c74a6e76fd4a381a7548886
rychanya <rychanya@gmail.com>
2022-07-24 00:27:19 +0300

    💩

--- bakeneko/web/routers/tg_web_app.py
+++ bakeneko/web/routers/tg_web_app.py
@@ -1,3 +1,6 @@
+import hashlib
+import hmac
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -13,9 +16,23 @@ templates = Jinja2Templates(directory="/code/bakeneko/templates")
 def root(request: Request):
     return templates.TemplateResponse(name="index.jinja", context={"request": request})
 
+
 @router.post("/")
 async def root_post(request: Request):
     json = request.query_params
+    data_check_string = "\n".join(
+        [
+            f"{k}={request.query_params[k]}"
+            for k in sorted(request.query_params.keys())
+            if k != "hash"
+        ]
+    )
+    secret_key = hmac.new("WebAppData".encode(), settings.TG_TOKEN.encode(), hashlib.sha256).digest()
+    data_check = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256).hexdigest()
     for k, v in json.items():
         print(f"{k} - {v}")
+    print(data_check_string)
+    print(secret_key)
+    print(data_check)
+    print(secret_key == json["hash"])
     return "ok"
