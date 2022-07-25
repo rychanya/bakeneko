969e18e185a1258764fada2948e2bcf3b5a46827
rychanya <rychanya@gmail.com>
2022-07-25 08:29:57 +0300

    💩

--- bakeneko/static/js/webmenu.js
+++ bakeneko/static/js/webmenu.js
@@ -1,15 +1,14 @@
 Telegram.WebApp.ready();
-Telegram.WebApp.MainButton.setText('Click').show().onClick(function () {
-    console.log(Telegram.WebApp.initDataUnsafe);
-    console.log(Telegram.WebApp.initData);
-    fetch("https://kittensanswers.ru/menu/?" + Telegram.WebApp.initData,
-        {
-            method: 'POST',
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
-        }
-    ).then(function (response) {
-        if (response.ok) {
-            Telegram.WebApp.close()
-        }
-    })
+Telegram.WebApp.MainButton.setText('Click').show().onClick(() => {
+  fetch(
+    `https://kittensanswers.ru/menu/?${Telegram.WebApp.initData}`,
+    {
+      method: 'POST',
+      headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+    },
+  ).then((response) => {
+    if (response.ok) {
+      Telegram.WebApp.close();
+    }
+  });
 });
